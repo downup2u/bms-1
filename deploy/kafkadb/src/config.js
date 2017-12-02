8fc533f28fec1cef95fc6fd81aaac51ddabf5e41
wangxiaoqing <xiaoqingwang@126.com>
2017-12-02 23:36:01 +0800

    for catl

--- deploy/kafkadb/src/config.js
+++ deploy/kafkadb/src/config.js
@@ -2,7 +2,7 @@ let config =  {
   mongodburl:process.env.MONGO_URL || 'mongodb://localhost/bms',
   consumerOptions:{
     host: process.env.KAFKA_HOST ||'bmscatl.com28.cn:2181',
-    groupId: 'BMSRecvGroup',
+    groupId: 'BMSRecvGroup2',
     sessionTimeout: 15000,
     protocol: ['roundrobin'],
     fromOffset: 'earliest' // equivalent of auto.offset.reset valid values are 'none', 'latest', 'earliest'
